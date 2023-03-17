ba37a891aa8ef17ade9be4f81743ca965001e003
Francis <francisbernal14@gmail.com>
2023-03-18 00:07:54 +0100

    feat: Set up mongoose configuration

--- index.js
+++ index.js
@@ -3,6 +3,12 @@ const app = express()
 const cors = require('cors')
 require('dotenv').config()
 
+// Set up database configuration (Mongoose)
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true });
+
+
 app.use(cors())
 app.use(express.static('public'))
 app.get('/', (req, res) => {
